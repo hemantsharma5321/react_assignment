76606f054a2454b8409e3083a94ef6e60bd98710
Hemant kumar sharma <Hkumar5321@gmail.com>
2022-11-05 20:51:53 +0530

    assignment

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,24 @@
+import ramsetu from "../images/RamSetu.jpg";
+import {TiHeartOutline} from "react-icons/ti";
+import { AiOutlineShareAlt} from "react-icons/ai";
+import { BsFillChatLeftFill} from "react-icons/bs";
 const MovieCard = (props) => {
   return (
     <div className="card">
-      {console.log(props.movie)}
+      
       <div style={{width:"60%"}}>
+       <img src={ramsetu} alt=" v" style={{width:"100px" , height:"150px", float:"left", margin:"5px"}}></img>
+       {console.log(eval(props.img))}
+       
         <h4>{props.movie.Title}</h4>
         <p>{props.movie.Year +", "+ props.movie.Director}</p>
         <p>{props.movie.Actors}</p>
+        <br/>
         <p>{props.movie.Plot}</p>
+        <br/>
+        <p><TiHeartOutline/>&nbsp;<AiOutlineShareAlt/>&nbsp;<BsFillChatLeftFill/></p>
       </div>
-      <div className="cardImg">image</div>
+      <div className="cardImg"><img src={ramsetu} alt="v"></img></div>
       
     </div>
      
